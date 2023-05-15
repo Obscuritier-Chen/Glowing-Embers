246b090b1dba6152cb2f79345c834fb4d9b7205c
Obscuritier-Chen <133127004+Obscuritier-Chen@users.noreply.github.com>
2023-05-15 22:14:17 +0800

    Add files via upload

---
+++ events.js
@@ -0,0 +1,131 @@
+﻿var randomEventsPr={event1:10,	
+					event2:10,
+				   	event3:10};
+var randomEventsAttribute={
+	event1:{//事件标题 内容 类型
+		title:'event1',
+		content:'population-5',
+		type:1
+	},
+	event2:{
+		title:'event2',
+		content:'product1+5',
+		type:1
+	},
+	event3:{
+		title:'event3',
+		content:'test3',
+		type:2
+	}
+};
+function performConfirmEvents(randomEventNum)
+{
+	switch (randomEventNum)//根据时间编号产生事件效果
+	{
+		case 1:
+			popSub(5);
+			break;
+		case 2:
+			production['product1Num']+=5;
+			elementPro['product1Num'].innerText=production['product1Num'];
+			break;
+		default:
+			break;
+	}
+}
+function randomEvents()
+{
+	var prSum=0,coefficient,randomEventNum;//randomEventNum是随机事件编号
+	var randomEventsLength=Object.keys(randomEventsPr).length;//获取字典的大小
+	//prPrefixSum
+	prPrefixSum=new Array(randomEventsLength+1);
+	prPrefixSum[0]=0;
+	var i=1;
+	for(var key in randomEventsPr)//和人口增加的思路类似，但概率是预设的
+	{
+		prSum+=randomEventsPr[key];
+		prPrefixSum[i]=Number(prPrefixSum[i-1]+randomEventsPr[key]);
+		i++;
+	}
+	coefficient=1/prSum;
+	coefficient=coefficient.toFixed(7);
+	coefficient=Number(coefficient);
+	for(var i=1;i<=randomEventsLength;i++)
+	{
+		prPrefixSum[i]*=coefficient;
+		prPrefixSum[i]=prPrefixSum[i].toFixed(7);
+		prPrefixSum[i]=Number(prPrefixSum[i]);
+	}
+	randomPr=Math.random();//随机生成0~1之间的数
+	for(var i=1;i<=randomEventsLength;i++)
+	{
+		if(prPrefixSum[i]>=randomPr)
+			{randomEventNum=i;break;}
+	}
+	return randomEventNum;
+}
+function eventsDisplay()
+{
+	if(document.getElementById('eventsPopup') == null)
+	{
+		var randomEventsNum=randomEvents();//产生时间编号
+		if(randomEventsAttribute['event'+randomEventsNum]['type']==1)
+		{
+			performConfirmEvents(randomEventsNum);//确认类型事件 的效果
+			var popup = document.createElement('div');
+    		popup.setAttribute('id', 'eventsPopup'); // 添加id
+    		popup.style.position = 'fixed';
+    		popup.style.top = '50%';
+    		popup.style.left = '50%';
+    		popup.style.transform = 'translate(-50%, -50%)';
+    		popup.style.backgroundColor = 'white';
+			popup.style.padding = '200px';
+			popup.style.border = '1px solid black';
+			popup.style.zIndex = '9999';
+			// create a new div element for the text
+			var titleDiv = document.createElement('div');//标题
+			titleDiv.innerText = randomEventsAttribute['event'+randomEventsNum]['title'];
+			titleDiv.style.position = 'absolute';
+			titleDiv.style.top = '10px';
+			//titleDiv.style.left = '50px';
+			titleDiv.style.fontSize = '22px';//字体大小
+			titleDiv.style.left = '0';
+			titleDiv.style.right = '0';
+			titleDiv.style.textAlign = 'center';
+			//titleDiv.style.wordWrap = 'break-word';//自动换行
+			//titleDiv.style.maxWidth = 'calc(100% - 90px)';//难不成你个标题还换行？
+			popup.appendChild(titleDiv);//将此文本加入到popup中
+			//------------------------------------分割线----------------------------------------
+			var contentDiv = document.createElement('div');//内容
+			contentDiv.innerText = randomEventsAttribute['event'+randomEventsNum]['content'];
+			contentDiv.style.position = 'absolute';
+			contentDiv.style.top = '70px';
+			contentDiv.style.left = '50px';
+			contentDiv.style.fontSize = '17px';//字体大小
+			contentDiv.style.wordWrap = 'break-word';//自动换行
+			contentDiv.style.maxWidth = 'calc(100% - 100px)';//距离右边界n px时换行,n=-50时达到右边界，
+			popup.appendChild(contentDiv);//将此文本加入到popup中
+			//------------------------------------分割线----------------------------------------
+			var confirmButton = document.createElement('button');
+			confirmButton.style.position = 'absolute';
+			confirmButton.style.bottom = '20px';
+			confirmButton.style.right = '20px';
+			confirmButton.style.background = 'none'; // 删除按钮背景
+			//confirmButton.style.border = 'none';// 删除按钮边框
+			confirmButton.style.fontSize = '1.5em';
+			confirmButton.innerText = "confirm";
+			//confirmButton.classList.add('cross'); // 创建关闭按钮内文本的class为cross
+			confirmButton.addEventListener('click', function() {
+				popup.remove(); // 点击关闭按钮时移除popup
+			});
+			popup.appendChild(confirmButton);
+			// 将popup添加到body中
+			document.body.appendChild(popup);
+		}
+		else
+		{
+			alert("waits");
+		}
+	}
+}
+//setInterval(eventsDisplay,eventSpeed);
---
+++ producing.js
@@ -0,0 +1,257 @@
+﻿var popSpeed=1000,proSpeed=2000,eventSpeed=5000;
+var population=0,popLimit=20,lastWorker=3;
+var production={product1Num:0,product2Num:20,product3Num:20,jobless:0};
+var building={building1:false,building2:false};
+var elementBtnAdd={worker1:document.getElementById('btn1ProAdd'),
+				   worker2:'xzx',
+				   worker3:'xzx'};
+var elementBtnSub={worker1:document.getElementById('btn1ProSub'),
+				   worker2:'xzx',
+				   worker3:'xzx'};
+var elementPro={product1Num:document.getElementById('product1s'),//在字典里就获取element,但此时body未加载
+				product2Num:document.getElementById('product2s'),
+				product3Num:document.getElementById('product3s'),
+			 	jobless:document.getElementById('jobless')};
+var worker={worker1:0,worker2:0,worker3:0};
+var elementWorkNum={worker1:document.getElementById('worker1'),
+				    worker2:'xzx',
+				    worker3:'xzx'};//有xzx的都是之后创建的元素，需要创建时赋值
+var bld2Num={building1:2,
+			 building2:3};
+var num2WkrName={building1:'worker2Num',
+			 	building2:'worker3Num'};
+var workersTable={// 注意此变量的一级下标worker1,worker2等等必须与worker完全相同
+	worker1:{//二级下标product1Num,product2Num等等必须与production完全相同
+		product1Num:5,
+		product2Num:0,
+		product3Num:0
+	},
+	worker2:{
+		product1Num:0,
+		product2Num:2,
+		product3Num:0
+	},
+	worker3:{
+		product1Num:0,
+		product2Num:-1,
+		product3Num:2
+	}
+};
+var buildingsTable={
+	building1:{
+		product1Num:0,
+		product2Num:5,
+		product3Num:0
+	},
+	building2:{
+		product1Num:0,
+		product2Num:1,
+		product3Num:5
+	}
+};
+function miuGenerate(x)//根据最大人口与当前人口差值生成μ
+{
+	var ans;
+	if(x>=1&&x<=15) ans=x-4.5;
+	else if(x>15) ans=10.5;
+	return ans;
+}
+function nd(x,miu)//正态分布函数，x为自变量，miu为μ(平均数)，即该曲线对称轴的位置
+{
+	var e=2.71828,sigma=3,y,sqrt2Pie=2.50663,index;//sigma标准差，e自然常数，index e的指数
+	index=-((x-miu)**2)/(2*(sigma**2));
+	y=(1/(sigma*sqrt2Pie))*e**index;
+	return y;
+}
+function initialization()//加载后运行
+{
+	document.getElementById('popNum').innerText=population;//初始化人口
+	document.getElementById('maxPop').innerText=popLimit;
+}
+function productions()
+{
+	for(const keyw in workersTable)//遍历table的worker
+	{
+		for(var i=1;i<=worker[keyw];i++)//按每个worker判定是否资源足够
+		{
+			var enoughProJudge=true;
+			for(const keyp in workersTable[keyw])//遍历所有种资源判断是否足够
+			{
+				if(production[keyp]+workersTable[keyw][keyp]<0)
+					{enoughProJudge=false;break;}
+			}
+			if(enoughProJudge)
+				for(const keyp in workersTable[keyw])
+					production[keyp]+=workersTable[keyw][keyp];
+		}
+	}//当初始worker!=0时可能会有bug?
+	for(var key in production)//同时遍历production
+	{
+		elementPro[key].innerText=production[key];
+	}
+	//product1Num=Math.max(product1Num+count,0);
+	return productions;
+}
+function popIncrement(differ)//计算人口增加量
+{//这玩意参数还是要调一调sigma变为3 differ=1时 x=-3.5 differ=8时 x=3.5 differ=15时x=10.5
+	var incrementPr=new Array(10);
+	var prPrefixSum=new Array(10);//前缀和
+	var incrementSum=0,coefficient,randomPr,increment;//coefficient系数，将概率之和转换为1的系数
+	for(var i=0;i<=7;i++)
+	{
+		incrementPr[i]=nd(i,miuGenerate(differ));//计算各人口增长量的概率
+		incrementPr[i]=incrementPr[i].toFixed(5);//只取小数点后5位
+		incrementPr[i]=Number(incrementPr[i]);//将字符型转换为数字
+		incrementSum=incrementPr[i]+incrementSum;
+	}
+	coefficient=1/incrementSum;//计算系数使总概率为1
+	coefficient=coefficient.toFixed(5);
+	coefficient=Number(coefficient);
+	prPrefixSum[0]=incrementPr[0]*coefficient;
+	for(var i=1;i<=7;i++)//计算前缀和
+	{
+		incrementPr[i]*=coefficient;
+		prPrefixSum[i]=prPrefixSum[i-1]+incrementPr[i];
+		prPrefixSum[i]=prPrefixSum[i].toFixed(5);
+		prPrefixSum[i]=Number(prPrefixSum[i]);
+	}//最终总概率为1(+/-)10^-5问题不大
+	randomPr=Math.random();//随机生成0~1之间的数
+	for(var i=0;i<=7;i++)
+	{
+		if(prPrefixSum[i]>=randomPr)
+			{increment=i;break;}
+	}
+	return increment;
+}
+function popUpdate()
+{
+	var deltapop=0;//现在人口增长速度看起来可能很怪，不必惊慌只是函数算出来增加量是0
+	if(popLimit-population>0)//否则有可能有奇怪的bug,直接限制比较方便
+	{
+		deltapop=popIncrement(popLimit-population);
+		deltapop=Math.min(popLimit-population,deltapop);//保证人口数量不超过人口限制
+	}
+	population+=deltapop;//计算人口量结果
+	production['jobless']+=deltapop;
+	document.getElementById('popNum').innerText=population;
+	elementPro['jobless'].innerText=production['jobless'];
+	return popUpdate;
+}
+function bldHouse()
+{
+	if(production['product2Num']>=5)
+	{
+		production['product2Num']-=5;//建房，扣资源，加人口限制
+		elementPro['product2Num'].innerText=production['product2Num'];
+		popLimit+=5;
+		document.getElementById('maxPop').innerText=popLimit;
+	}
+}
+function WorkersAdd(AddorSub,name)
+{
+	//工人+1 +5 -1 -5的情况
+	if(AddorSub==1)
+	{
+		if(production['jobless']>0)
+			worker[name]++,production['jobless']--;
+	}
+	else if(AddorSub==5)
+	{
+		if(production['jobless']>=5)
+			worker[name]+=5,production['jobless']-=5;
+	}
+	else if(AddorSub==-1)
+	{
+		if(worker[name]>0)
+			worker[name]--,production['jobless']++;
+	}
+	else if(AddorSub==-5)
+	{
+		if(worker[name]>=5)
+			worker[name]-=5,production['jobless']+=5;
+	}
+	elementPro['jobless'].innerText=production['jobless'];
+	worker[name]=Math.max(worker[name],0);//似乎没用
+	elementWorkNum[name].innerText=worker[name];
+	/*if(production['jobless']==0)//从这里
+	{
+		/*for(var key in worker)
+		{
+			elementBtnAdd[key].disabled=true;
+		}
+		//add.style.background= "grey";
+		const addBefore = document.querySelector('.add::before');
+		const addAfter = document.querySelector('.add::after');
+		addBefore.style.background = 'grey';
+		addAfter.style.background = 'grey';
+	}
+	if(production['jobless']>0)
+	{
+		for(var key in worker)
+		{
+			elementBtnAdd[key].disabled=false;
+		}
+	}
+	if(worker[name]==0)
+			elementBtnSub[name].disabled=true;
+	if(worker[name]>0)
+			elementBtnSub[name].disabled=false;//到这里是判断按钮是否需要无效/解除无效
+	*/
+}
+function popSub(reduction)
+{
+	reduction=Math.min(population,reduction);
+	population-=reduction;//减少总人口
+	document.getElementById('popNum').innerText=population;
+	var temp=Math.min(production['jobless'],reduction);
+	production['jobless']-=Math.min(production['jobless'],reduction);//优先减无业者
+	reduction-=temp;//计算新的减少量
+	elementPro['jobless'].innerText=production['jobless'];
+	for(var i=lastWorker;i>=1;i--)
+	{
+		workerName='worker'+i;//确定减少的工人
+		var temp=Math.min(worker[workerName],reduction);
+		worker[workerName]-=Math.min(worker[workerName],reduction);//减少该工人
+		reduction-=temp;
+		elementWorkNum[workerName].innerText=worker[workerName];
+	}
+}
+function build(name)
+{
+	var enoughResources=true;
+	if(!building[name])
+	{
+		for (var key in buildingsTable[name])//确认资源足够建造
+		{
+			if(buildingsTable[name][key]>production[key])
+				{enoughResources=false;break;}
+		}
+		if(enoughResources)//cursor写的有些看不懂，呜呜呜
+		{//创建新的worker元素
+			var num=bld2Num[name];
+			building[name]=true;
+			var workerNum=0;
+			var workerName=num2WkrName[name];
+			var workerDiv=document.createElement('div');
+			workerDiv.setAttribute('id','worker'+num+'s');
+			workerDiv.innerHTML=workerName+//不要换行，有莫名其妙的bug。也不要改，调语法累死
+								'： <button class="btnSubClass" onclick="WorkersAdd(-5,'+'\'worker'+num+'\')"><span class="sub"></span></button><button class="btnSubClass" onclick="WorkersAdd(-1,'+'\'worker'+num+'\')"><span class="sub"></span></button>\n<span id="worker'+num+'">'
+								+workerNum+'</span>\n<button class="btnAddClass" onclick="WorkersAdd(1,'+'\'worker'+num+'\')"><span class="add"></span></button><button class="btnAddClass" onclick="WorkersAdd(5,'+'\'worker'+num+'\')"><span class="add"></span></button>';
+			var workerDivs=document.querySelectorAll('div[id$="s"]');
+			document.body.insertBefore(workerDiv,document.getElementById("workerLast"));/*在worker的最后
+			有一个workerLast标签，标记了worker的底线，新的worker从此前插入*/
+			elementWorkNum['worker'+num]=document.getElementById('worker'+num);
+			/*elementBtnAdd['worker'+num]=document.getElementById('btn'+num+'ProAdd')
+			elementBtnSub['worker'+num]=document.getElementById('btn'+num+'ProSub')//将id赋值给数组*/
+		}
+		if(enoughResources)//扣资源
+			for (var key in buildingsTable[name])
+				production[key]-=buildingsTable[name][key];
+		for(var key in production)//建造完成后刷新资源显示
+		{
+			elementPro[key].innerText=production[key];
+		}
+	}
+}
+setInterval(productions(),proSpeed);//定期执行production
+setInterval(popUpdate,popSpeed);
